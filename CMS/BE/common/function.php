508bbe435ec565bd31c364951139572a22431ceb
RSS-26\RSS <fatima.hassouna@lau.edu>
2024-05-07 11:19:30 +0300

    connection with the database for CMS and public_website

---
+++ CMS/BE/common/function.php
@@ -0,0 +1,43 @@
+<?php
+// this function is to connect the files with the database
+function DBConncet(){
+    $dbhost="127.0.0.1";
+    $dbname="db_final";
+    $dbuser="root";
+    $dbpass=NULL;
+    $db=null;
+ 
+    try{
+     $db = new PDO("mysql:host=$dbhost; dbname=$dbname",$dbuser, $dbpass);
+    }catch(PDOException $e){
+     print "Error!: " . $e->getMessage() . "<br/>";
+     die();
+     $db=null;
+    }
+    return $db;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
