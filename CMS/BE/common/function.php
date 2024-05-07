f092c408b654f61c2b514588db14755c9d986ab3
RSS-26\RSS <fatima.hassouna@lau.edu>
2024-05-07 12:07:19 +0300

    added isExist function to the common/function file

--- CMS/BE/common/function.php
+++ CMS/BE/common/function.php
@@ -1,23 +1,31 @@
 <?php
 // this function is to connect the files with the database
-function DBConncet(){
-    $dbhost="127.0.0.1";
-    $dbname="db_final";
-    $dbuser="root";
-    $dbpass=NULL;
-    $db=null;
- 
-    try{
-     $db = new PDO("mysql:host=$dbhost; dbname=$dbname",$dbuser, $dbpass);
-    }catch(PDOException $e){
-     print "Error!: " . $e->getMessage() . "<br/>";
-     die();
-     $db=null;
+    function DBConncet(){
+        $dbhost="127.0.0.1";
+        $dbname="db_final";
+        $dbuser="root";
+        $dbpass=NULL;
+        $db=null;
+    
+        try{
+        $db = new PDO("mysql:host=$dbhost; dbname=$dbname",$dbuser, $dbpass);
+        }catch(PDOException $e){
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+        $db=null;
+        }
+        return $db;
     }
-    return $db;
-}
 
 
+
+    function VarExist($var){
+        if (isset($var)){
+            return $var;
+        }else{
+            header("location:../index.html");
+        }
+    }
 
 
 
