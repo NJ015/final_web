b06e21c120b6ee44aedcf88ec40c877c8de419ec
RSS-26\RSS <fatima.hassouna@lau.edu>
2024-05-07 12:07:53 +0300

    implementing the add_jewelry function

--- CMS/BE/jewelry.php
+++ CMS/BE/jewelry.php
@@ -1,17 +1,19 @@
 <?php
 
-
-function add(){
-
-}
-
-function activate(){
-
-}
-
-function deactivate(){
-    
-}
+    require_once("common/function.php");
+    $obj = new stdClass();
+     $obj->name=VarExist($_POST["name"]);
+     $obj->type=VarExist($_POST["type"]);
+     $obj->price=VarExist($_POST["price"]);
+    add_jewelry($obj);
+    function add_jewelry($obj){
+       $db = DBConncet();
+       $query="INSERT INTO tbl_jewelry (`NAME`,`TYPE`,`PRICE`) VALUES ('$obj->name','$obj->type','$obj->price')";
+       $stmt=$db->query($query);
 
 
+    }
+
+
+   
 ?>
